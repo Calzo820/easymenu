8f72bec2eaad29a7a6200f02831b3851776540a1
Lorenzo <easy.menu.service@gmail.com>
2026-05-29 22:24:13 +0200

    disable stale service worker

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,12 @@ import "./styles/easymenu-os.css";
 import "./styles/easymenu-ux-pro.css";
 import "./styles/operational-ux.css";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
